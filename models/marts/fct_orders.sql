fe32ee659a21580a1badb324dd68ebd22557d02b
RanaMKDev <rmahmoudkamel@gmail.com>
2026-05-01 16:09:31 +0100

    Update fct_orders.sql

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,6 +1,6 @@
 select 
   CustomerName, OrderDate, OrderID 
 from 
-  DatabricksWarehouse.Sales.Customers C 
-  inner join DatabricksWarehouse.Sales.Orders O 
+  `samples`.`bakehouse`.`sales_customers` C 
+  inner join `samples`.`bakehouse`.`sales_orders` O 
   on C.CustomerID = O.CustomerID

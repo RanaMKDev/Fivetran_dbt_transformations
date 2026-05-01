c2e34df7bbddb65a913553a5619c334cd7421442
RanaMKDev <rmahmoudkamel@gmail.com>
2026-05-01 16:09:06 +0100

    Update transformationmodel.sql

--- models/staging/transformationmodel.sql
+++ models/staging/transformationmodel.sql
@@ -5,8 +5,8 @@ SELECT
     YEAR(O.OrderDate) AS OrderYear,
     MONTH(O.OrderDate) AS OrderMonth,
     COUNT(O.OrderID) AS OrderCount
-FROM Sales.Customers C
-INNER JOIN Sales.Orders O 
+FROM `samples`.`bakehouse`.`sales_customers` C
+INNER JOIN `samples`.`bakehouse`.`sales_orders` O 
     ON C.CustomerID = O.CustomerID
 GROUP BY 
     C.CustomerName,

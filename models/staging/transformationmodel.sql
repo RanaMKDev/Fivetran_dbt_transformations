145738e975c9db140eb27d7d8b98e873f1c79534
RanaMKDev <rmahmoudkamel@gmail.com>
2026-05-01 16:03:57 +0100

    Update transformationmodel.sql

--- models/staging/transformationmodel.sql
+++ models/staging/transformationmodel.sql
@@ -2,18 +2,17 @@
     
 SELECT
     C.CustomerName,
-    DATEPART(YEAR, O.OrderDate) AS OrderYear,
-    DATEPART(MONTH, O.OrderDate) AS OrderMonth,
+    YEAR(O.OrderDate) AS OrderYear,
+    MONTH(O.OrderDate) AS OrderMonth,
     COUNT(O.OrderID) AS OrderCount
-FROM DatabricksWarehouse.Sales.Customers C
-INNER JOIN DatabricksWarehouse.Sales.Orders O 
+FROM Sales.Customers C
+INNER JOIN Sales.Orders O 
     ON C.CustomerID = O.CustomerID
 GROUP BY 
     C.CustomerName,
-    DATEPART(YEAR, O.OrderDate),
-    DATEPART(MONTH, O.OrderDate)
+    YEAR(O.OrderDate),
+    MONTH(O.OrderDate)
 ORDER BY 
     C.CustomerName,
     OrderYear,
-    OrderMonth;
-
+    OrderMonth

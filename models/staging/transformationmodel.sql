aa1180020525cf54ad5f4ef3c7e2770cc99fcc5a
RanaMKDev <rmahmoudkamel@gmail.com>
2026-05-01 13:27:47 +0100

    Create transformationmodel.sql
    transformationmodel.sql

---
+++ models/staging/transformationmodel.sql
@@ -0,0 +1,18 @@
+Create OR Replace View OrderperCuastomers as (
+SELECT
+    C.CustomerName,
+    DATEPART(YEAR, O.OrderDate) AS OrderYear,
+    DATEPART(MONTH, O.OrderDate) AS OrderMonth,
+    COUNT(O.OrderID) AS OrderCount
+FROM Sales.Customers C
+INNER JOIN Sales.Orders O 
+    ON C.CustomerID = O.CustomerID
+GROUP BY 
+    C.CustomerName,
+    DATEPART(YEAR, O.OrderDate),
+    DATEPART(MONTH, O.OrderDate)
+ORDER BY 
+    C.CustomerName,
+    OrderYear,
+    OrderMonth;
+)
